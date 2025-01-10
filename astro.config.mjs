3781b5d717e4d89866ca7772bba2f6ffaccdca2d
Aileen Villanueva <aileenvl@gmail.com>
2025-01-10 15:07:39 -0600

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [react(), tailwind()],
-  vite: {
-    define: {
-      'process.env.PUBLIC_ORAMA_ENDPOINT': JSON.stringify(process.env.PUBLIC_ORAMA_ENDPOINT),
-      'process.env.PUBLIC_ORAMA_API_KEY': JSON.stringify(process.env.PUBLIC_ORAMA_API_KEY)
-    }
-  }
+  integrations: [react(), tailwind()]
 });
