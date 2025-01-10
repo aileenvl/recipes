51a242b918bd72379f4493282f1975975ad27487
Aileen Villanueva <aileenvl@gmail.com>
2025-01-10 12:31:12 -0600

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
