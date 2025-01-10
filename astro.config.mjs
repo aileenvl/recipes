5ba50aa0e0b93a6e1e6116f997f403faf13ff07d
Aileen Villanueva <aileenvl@gmail.com>
2025-01-10 12:10:30 -0600

    fixing vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
@@ -10,6 +10,7 @@ const adapter = process.env.VERCEL ? vercel({
   webAnalytics: true,
   speedInsights: true,
   imageService: true,
+  devMode: true,
 }) : node({
   mode: 'standalone'
 });
