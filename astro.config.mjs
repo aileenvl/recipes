7fb78ed4fab76a483e76cdbe3e4072bba57596c7
Aileen Villanueva <aileenvl@gmail.com>
2025-01-10 15:54:16 -0600

    rollback to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,7 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    maxDuration: 60
-  }),
+  output: 'hybrid',
+  adapter: vercel(),
   integrations: [react(), tailwind()]
 });
