8802b1e93952cc55e01253bf27a25a55fa3a2ac4
Aileen Villanueva <aileenvl@gmail.com>
2025-01-10 15:12:27 -0600

    fix error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
+  output: 'static',
   integrations: [react(), tailwind()]
 });
