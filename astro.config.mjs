3cd11cfc0397da2b78c20dc15e2e22a64c03f020
Aileen Villanueva <aileenvl@gmail.com>
2025-01-10 15:16:07 -0600

    use hybrid output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'static',
+  output: 'hybrid',
+  adapter: vercel(),
   integrations: [react(), tailwind()]
 });
