8c5a52c510f95fd75b81c6e4ddb52a55ee62f4fa
Aileen Villanueva <aileenvl@gmail.com>
2025-01-18 16:13:59 -0600

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,21 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
-    imagesConfig: {
-      sizes: [640, 750, 828, 1080, 1200],
-      domains: [],
-      minimumCacheTTL: 60,
-    },
-    imageService: true,
-    devImageService: 'sharp',
+    maxDuration: 8,
+    isr: {
+      enabled: false
+    }
   }),
-  integrations: [react(), tailwind()],
-  build: {
-    format: 'directory'
-  }
+  integrations: [
+    react(),
+    tailwind()
+  ]
 });
