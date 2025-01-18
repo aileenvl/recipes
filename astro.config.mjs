6eac9774c9289285f9aa8dabb5d956e2fb91edbf
Aileen Villanueva <aileenvl@gmail.com>
2025-01-17 20:57:30 -0600

    add vercel routing cofig

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,17 @@ export default defineConfig({
   adapter: vercel({
     webAnalytics: {
       enabled: true,
-    }
+    },
+    imagesConfig: {
+      sizes: [640, 750, 828, 1080, 1200],
+      domains: [],
+      minimumCacheTTL: 60,
+    },
+    imageService: true,
+    devImageService: 'sharp',
   }),
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  build: {
+    format: 'directory'
+  }
 });
